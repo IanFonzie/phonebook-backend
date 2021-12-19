88d047cda947ef51893e1256fb1f20429f872db6
IanFonzie <fberg_i@hotmail.com>
2021-12-19 13:14:48 -0800

    Complete exercise 3.2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (req, res, next) => {
   return res.json(persons)
 })
 
+app.get('/info', (req, res, next) => {
+  const body = `<p>Phone book has info for ${persons.length} people.</p>` +
+  `<p>${new Date()}</p>`
+  res.send(body)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

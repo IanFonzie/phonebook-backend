5ee32320f7f2ec517d1d77a4fa906b5b253df7af
IanFonzie <fberg_i@hotmail.com>
2021-12-23 15:19:26 -0800

    Complete exercise 3.18

--- index.js
+++ index.js
@@ -13,29 +13,6 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (req, res, next) => {
   Person.find({}).then(persons => {
     return res.json(persons)
@@ -43,9 +20,11 @@ app.get('/api/persons', (req, res, next) => {
 })
 
 app.get('/info', (req, res, next) => {
-  const body = `<p>Phone book has info for ${persons.length} people.</p>` +
-  `<p>${new Date()}</p>`
-  res.send(body)
+  Person.find({}).then(persons => {
+    const respBody = `<p>Phone book has info for ${persons.length} people.</p>` +
+    `<p>${new Date()}</p>`
+    res.send(respBody)
+  })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
